6113b437bf34f160e26e265e72b400e0073310e8
Nikita Ahuja <nahuja038@gmail.com>
2022-10-12 15:48:13 -0500

    added dropdown and homepage aye

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-var pages = ["bachata-footwork-1.html", ]
+// var pages = ["bachata-footwork-1.html", ]
+
+
+
+<script type="text/javascript">
+    function goToNewPage()
+    {
+        var url = document.getElementById('list').value;
+        if(url != 'none') {
+            window.location = url;
+        }
+    }
+</script>
