09a18bf031f19047b18691148398b22389ada5a2
Nikita Ahuja <nahuja038@gmail.com>
2022-10-12 15:58:57 -0500

    copied js to js file

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
 // var pages = ["bachata-footwork-1.html", ]
 
-
-
-<script type="text/javascript">
-    function goToNewPage()
-    {
-        var url = document.getElementById('list').value;
-        if(url != 'none') {
-            window.location = url;
+function goToNewPage()
+        {
+            var url = document.getElementById('list').value;
+            if(url != 'none') {
+                window.location = url;
+            }
         }
-    }
-</script>
+
+
