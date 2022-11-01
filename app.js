31117dd3c1445c6f2b7b19febf37345c9897db1a
Nikita Ahuja <nahuja038@gmail.com>
2022-10-31 20:44:34 -0500

    add js to create nav bar

--- app.js
+++ app.js
@@ -6,4 +6,11 @@ function goToNewPage()
             }
         }
 
-
+        fetch('nav.html')
+        .then(res => res.text())
+        .then(text => {
+            let oldelem = document.querySelector("script#replace_with_navbar");
+            let newelem = document.createElement("div");
+            newelem.innerHTML = text;
+            oldelem.parentNode.replaceChild(newelem,oldelem);
+        })
